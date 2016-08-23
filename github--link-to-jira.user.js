97b3ad3c7ab36b84ce11a5f4c7521015d3bf46e3
Duncan Chung <duncan.e.chung@gmail.com>
2016-08-23 14:14:44 +1000

    add github script - link to jira

---
+++ github--link-to-jira.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        Link to Jira from Github
+// @namespace   https://github.com/duncanchung/greasemonkey/
+// @description Make Jira ids linkable from Github
+// @author      duncanchung
+// @copyright   2016
+// @license     GNU GPLv3
+// @homepage    https://github.com/duncanchung/greasemonkey
+// @homepageURL https://github.com/duncanchung/greasemonkey
+// @supportURL  https://github.com/duncanchung/greasemonkey/issues
+// @version     1
+// @grant       none
+// @include     https://github.com/*
+// ==/UserScript==
+(function () {
+  function init() {
+    var jiraRegex = /\b([A-Z]+-[0-9]+)\b/g;
+    var itemsToCheck = new Array();
+    document.querySelectorAll('span.js-issue-title').forEach(function (e) {
+      itemsToCheck.push(e);
+    });
+    document.querySelectorAll('a.message').forEach(function (e) {
+      itemsToCheck.push(e);
+    });
+    itemsToCheck.forEach(function (e) {
+      if (jiraRegex.test(e.innerHTML)) {
+        e.innerHTML = e.innerHTML.replace(jiraRegex, '<a href="https://domain.atlassian.net/browse/$1">$1</a>');
+      }
+    });
+  }  // Page load;
+
+  init();
+  // On pjax;
+  document.addEventListener('pjax:end', init);
+}) ();
